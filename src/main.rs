04577de889bacd6bfbc34a3837698c9f626cd2f5
Will Bradley <williambbradley@gmail.com>
2024-04-21 22:59:51 -0600

    updates

--- src/main.rs
+++ src/main.rs
@@ -1,64 +1,137 @@
+use actix_web::error::ResponseError;
 use actix_web::rt::time::sleep;
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 use rand::Rng;
 use serde::{Deserialize, Serialize};
-use sha2::{Digest, Sha256};
-use std::time::{Duration, SystemTime, UNIX_EPOCH};
-const SALT: &str = "ab39ncf0-3aldmAS3NK3f";
+use serde_json::{Map, Value};
+use std::fmt;
+use std::time::Duration;
+
+use reqwest::Client;
+
+#[derive(Debug)]
+enum Error {
+    NotFound,
+    Internal,
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+impl ResponseError for Error {
+    fn error_response(&self) -> HttpResponse {
+        match *self {
+            Error::NotFound => HttpResponse::NotFound().finish(),
+            Error::Internal => HttpResponse::InternalServerError().finish(),
+        }
+    }
+}
+
+impl From<reqwest::Error> for Error {
+    fn from(e: reqwest::Error) -> Self {
+        log::error!("saw error: {e:?}");
+        Self::Internal
+    }
+}
 
 #[derive(Deserialize, Debug)]
 struct Request {
-    text: String,
+    quote: String,
+    base: String,
 }
 
 #[derive(Serialize)]
 struct Response {
-    text: String,
-    hash: String,
-    epoch_secs: u64,
+    date: String,
+    rate: f64,
+    quote: String,
+    base: String,
 }
 
-async fn handler(req_body: web::Json<Request>) -> impl Responder {
+async fn quote_handler(req_body: web::Json<Request>) -> Result<impl Responder, Error> {
     log::info!("handling request: {req_body:?}");
     let mut rng = rand::thread_rng();
     let prob: f64 = rng.gen();
 
     // Simulate failure 70% of the time
-    if prob < 0.7 {
-        return HttpResponse::InternalServerError().body("Internal Server Error");
+    if prob < 0.3 {
+        return Err(Error::Internal);
+        // HttpResponse::InternalServerError().body("Internal Server Error");
     }
-
-    let epoch_secs = SystemTime::now()
-        .duration_since(UNIX_EPOCH)
-        .expect("Time went backwards")
-        .as_secs();
 
     // Simulate delay 20% of the time
-    if prob < 0.9 {
+    if prob < 0.6 {
         sleep(Duration::from_secs(2)).await;
     }
-
-    let hash_text = format!("{}|{}|{}", SALT, req_body.text, epoch_secs);
-    let mut hasher = Sha256::new();
-    hasher.update(hash_text.as_bytes());
-    let hash_result = hasher.finalize();
-    let hash_hex = format!("{:x}", hash_result);
+    let (date, rate) = get_rate_quote(&req_body.quote, &req_body.base).await?;
 
     let resp = Response {
-        text: req_body.text.clone(),
-        hash: hash_hex,
-        epoch_secs,
+        date,
+        rate,
+        quote: req_body.quote.clone(),
+        base: req_body.base.clone(),
     };
 
-    HttpResponse::Ok().json(resp)
+    Ok(HttpResponse::Ok().json(resp))
+}
+
+async fn get_rate_quote(quote: &str, base: &str) -> Result<(String, f64), Error> {
+    // Simulate delay 20% of the time
+    let client = Client::new();
+    let json: Value = client
+        .get(format!("https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/{quote}.json"))
+        .header("Content-Type", "application/json")
+        .header("Accept", "application/json")
+        .send()
+        .await
+        .map_err(|_| Error::NotFound)?
+        .json::<Value>()
+        .await?;
+    let Value::Object(map) = json else {
+        log::error!("failed to get object from forex results");
+        return Err(Error::Internal);
+    };
+    let date: &str = map
+        .get("date")
+        .ok_or(Error::Internal)?
+        .as_str()
+        .unwrap_or("<unknown>");
+    let quotes: &Map<String, Value> = map
+        .get(quote)
+        .ok_or(Error::Internal)?
+        .as_object()
+        .ok_or(Error::Internal)?;
+    let rate: f64 = quotes
+        .get(base)
+        .ok_or(Error::Internal)?
+        .as_f64()
+        .ok_or(Error::Internal)?;
+    Ok((date.to_string(), rate))
 }
 
 #[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    simple_logging::log_to_file("/opt/shared/flakysaas.log", log::LevelFilter::Info)?;
-
-    HttpServer::new(|| App::new().route("/", web::post().to(handler)))
-        .bind("0.0.0.0:9001")?
+async fn main() {
+    // simple_logging::log_to_file("/opt/shared/flakysaas.log", log::LevelFilter::Info).unwrap();;
+    simple_logging::log_to_stderr(log::LevelFilter::Info);
+    let client = Client::new();
+    let text = client
+        .get("https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/btc.json")
+        .header("Content-Type", "application/json")
+        .header("Accept", "application/json")
+        .send()
+        .await
+        .unwrap()
+        .text()
+        .await
+        .unwrap();
+    log::info!("Test query: {text}");
+    HttpServer::new(|| App::new().route("/quote", web::post().to(quote_handler)))
+        .bind("0.0.0.0:9001")
+        .unwrap()
         .run()
         .await
+        .unwrap();
 }
